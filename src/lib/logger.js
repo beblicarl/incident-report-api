170d3cd7fcdaaac6bf1a9d4ea721a20796fc65a6
Carl Bebli <beblicarl.cb@gmail.com>
2022-12-13 13:52:47 +0000

    uncommented the code and corrected the errors

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,26 +1,27 @@
-// const chalk = require('chalk')
+const chalk = require('chalk')
 
-// const error = chalk.red
-// const warn = chalk.hex('#FFA500') // Orange color
-// const info = chalk.blue
-// const success = chalk.green
-// /* eslint-disable no-console */
-// const logger = () => ({
-// 	error(message) {
-// 		console.log(error(message))
-// 	},
+const error = chalk.red
+const warn = chalk.hex('#FFA500') // Orange color
+const info = chalk.blue
+const success = chalk.green
 
-// 	warn(message) {
-// 		console.log(warn(message))
-// 	},
+/* eslint-disable no-console */
+const logger = () => ({
+	error(message) {
+		console.log(error(message))
+	},
 
-// 	info(message) {
-// 		console.info(info(message))
-// 	},
+	warn(message) {
+		console.log(warn(message))
+	},
 
-// 	success(message){
-// 		console.log(success(message))
-// 	}
-// })
+	info(message) {
+		console.info(info(message))
+	},
 
-// module.exports = logger
+	success(message){
+		console.log(success(message))
+	}
+})
+
+module.exports = logger

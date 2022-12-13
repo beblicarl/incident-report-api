2130f00dd8b0b17d7ab4322f1987db3a7c57fc18
Carl Bebli <beblicarl.cb@gmail.com>
2022-12-13 13:53:43 +0000

    used logger in the server.js file

--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
-/* eslint-disable no-console */
 const app = require('./app')
-// const { logger } = require('./lib/index')
+const { logger } = require('./lib/index')
 
-// const log = logger()
+const log = logger()
 
 process.on('uncaughtExceptions', (err) => {
-	console.error(err.stack)
-	console.error('Uncaught Exceptions! Shutting down...')
+	log.error(err.stack)
+	log.error('Uncaught Exceptions! Shutting down...')
 	process.exit(1)
 })
 
 const port = process.env.PORT || 3000
 const server = app.listen(port, () => {
-	console.info(`App running on port ${port}...`)
+	log.info(`App running on port ${port}...`)
 })
 
 process.on('unhandledRejection', (err) => {
-	console.error(err.stack)
-	console.error('Unhandled Rejection! Shutting down...')
+	log.error(err.stack)
+	log.error('Unhandled Rejection! Shutting down...')
 	server.close(() => {
 		process.exit(1)
 	})

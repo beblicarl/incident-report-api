3e83e7d5366f59d6881e9ca62e06f2f135d10f15
Carl Bebli <beblicarl.cb@gmail.com>
2022-12-13 15:36:34 +0000

    exported custom error

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,11 @@
 const logger = require('./logger')
 const catchAsync = require('./catch-async')
 const AppError = require('./app-error')
+const customError = require('./custom-error')
 
 module.exports = {
 	logger,
 	catchAsync,
-	AppError
+	AppError,
+	customError
 }

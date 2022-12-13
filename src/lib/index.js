0a9eb5f8bcb7e93fd27f453a1ea5f313472292ec
Carl Bebli <beblicarl.cb@gmail.com>
2022-12-13 10:06:25 +0000

    created a lib folder to include custom libraries

---
+++ src/lib/index.js
@@ -0,0 +1,9 @@
+const logger = require('./logger')
+const catchAsync = require('./catch-async')
+const AppError = require('./app-error')
+
+module.exports = {
+	logger,
+	catchAsync,
+	AppError
+}
---
+++ src/lib/logger.js
@@ -0,0 +1,26 @@
+// const chalk = require('chalk')
+
+// const error = chalk.red
+// const warn = chalk.hex('#FFA500') // Orange color
+// const info = chalk.blue
+// const success = chalk.green
+// /* eslint-disable no-console */
+// const logger = () => ({
+// 	error(message) {
+// 		console.log(error(message))
+// 	},
+
+// 	warn(message) {
+// 		console.log(warn(message))
+// 	},
+
+// 	info(message) {
+// 		console.info(info(message))
+// 	},
+
+// 	success(message){
+// 		console.log(success(message))
+// 	}
+// })
+
+// module.exports = logger

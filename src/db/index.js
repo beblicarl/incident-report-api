199fd2093ec4478f4e4977d5bc8e9e80de9e76eb
Carl Bebli <beblicarl.cb@gmail.com>
2022-12-13 13:53:17 +0000

    imported logger and used it in the file

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,13 @@
-/* eslint-disable no-console */
 const { Pool } = require('pg')
 require('dotenv').config()
-// const logger = require('../lib/logger')
+const { logger } = require('../lib')
 
-// const log = logger()
-console.log(process.env.DATABASE_URL)
+const log = logger()
+
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL
 })
-console.log('database successfully connected')
+log.success('database successfully connected')
 module.exports = {
 	async query(text, params) {
 		const res = await pool.query(text, params)

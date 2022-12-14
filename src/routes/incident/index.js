f24f7c35d026c7625f1ba8346e94c60de068b684
Carl Bebli <beblicarl.cb@gmail.com>
2022-12-14 10:52:01 +0000

    replaced catchAsycn with trycatch

--- src/routes/incident/index.js
+++ src/routes/incident/index.js
@@ -11,7 +11,7 @@ const {
 
 const validateSchema = require('../../middleware/validateSchema')
 const { 
-	catchAsync,
+	logger,
 	AppError
 } = require('../../lib')
 
@@ -20,51 +20,60 @@ const ERROR_MAP = {
 	[IncidentReportDoesNotExistError.name] : 404
 	
 }
-
+const log = logger()
 const router = express.Router()
 
-const createIncidentReport = catchAsync( async (req, res) => {
+const createIncidentReport =  async (req, res, next) => {
 	
-	
-	const { incidentDescription, country, city } = req.body
-	const newReport = await reportService.createReport({
-		incidentDescription,
-		country, 
-		city
-	})
-	res.status(201).json({
-		status: 'success',
-		data: {
-			message: 'Incident report successfully posted',
-			clientId : newReport.client_id,
-			incidentDescription: newReport.incident_desc,
-			city: newReport.city,
-			country: newReport.country,
-			weatherReport: newReport.weather_report
-		}
-	})
-	
-})
-
-const fetchIncidents = catchAsync( async(req, res) => {
-	
-	const feed = await reportService.fetchIncidents()
-
-	res.status(200).json({
-		status: 'success',
-		data: feed.map((incident) => ({
-			clientId : incident.client_id,
-			incidentDescription : incident.incident_desc,
-			city: incident.city,
-			country: incident.country,
-			date: incident.date,
-			weatherReport: incident.weather_report
-
+	try {
+		const { incidentDescription, country, city } = req.body
+		const newReport = await reportService.createReport({
+			incidentDescription,
+			country, 
+			city
 		})
-		)
-	})
+		res.status(201).json({
+			status: 'success',
+			data: {
+				message: 'Incident report successfully posted',
+				clientId : newReport.client_id,
+				incidentDescription: newReport.incident_desc,
+				city: newReport.city,
+				country: newReport.country,
+				weatherReport: newReport.weather_report
+			}
+		})
+	} catch (error) {
+		log.error(error.message)
+		next(error)
+	}
+
+}
+
+const fetchIncidents =  async(req, res, next) => {
+	try {
+		const feed = await reportService.fetchIncidents()
+
+		res.status(200).json({
+			status: 'success',
+			data: feed.map((incident) => ({
+				clientId : incident.client_id,
+				incidentDescription : incident.incident_desc,
+				city: incident.city,
+				country: incident.country,
+				date: incident.date,
+				weatherReport: incident.weather_report
+
+			})
+			)
+		})
+	} catch (error) {
+		log.error(error.message)
+		next(error)
+	}
 	
-})
+	
+}
 
 
 router

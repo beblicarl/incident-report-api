102d99cfc39c261b380d891bd86dda5b715e39c7
Carl Bebli <beblicarl.cb@gmail.com>
2022-12-14 04:00:32 +0000

    improved the global error handler file

--- src/middleware/global-errorhandler.js
+++ src/middleware/global-errorhandler.js
@@ -1,12 +1,26 @@
 /* eslint-disable no-unused-vars */
 
+const { logger } = require('../lib')
+
+const log = logger()
+
+const sendError = (error, res) => {
+	if (error.isOperational) {
+		res.status(error.statusCode).json({
+			status: error.status,
+			message: error.message
+		})
+	} else {
+		// handle programming or unknown errors to clients
+		log.error(error)
+		res.status(500).json({
+			status: 'internal server error',
+			message: 'Something went wrong!'
+		})
+	}
+}
+
 module.exports = (error, req, res, next) => {
 	
-	res.status(error.status || 500).send({
-		error: {
-			status: error.status || 500,
-			message: error.message || 'Internal Server Error'
-		}
-	})
-	
+	sendError(error, res)
 }

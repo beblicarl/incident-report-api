8fedb9183a171b0438bd51bdd07f5ac902056bdd
Carl Bebli <beblicarl.cb@gmail.com>
2022-12-14 10:51:42 +0000

    took out catchAsync

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,9 @@
 const logger = require('./logger')
-const catchAsync = require('./catch-async')
 const AppError = require('./app-error')
 const customError = require('./custom-error')
 
 module.exports = {
 	logger,
-	catchAsync,
 	AppError,
 	customError
 }
